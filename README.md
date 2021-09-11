9464554925bbb13dd7f0a10c659fab60c851b982
Emre Guneyler <eguneys@gmail.com>
2021-09-11 13:52:00 +0000

    vision

---
+++ README.md
@@ -0,0 +1,5 @@
+## Awesome Haskell
+
+[What I Wish I Knew Long Reference](http://dev.stephendiehl.com/hask/)
+
+[Composing Applicatives vs Monads](https://stackoverflow.com/questions/29453915/composing-monads-v-applicative-functors)
--- src/Pos.hs
+++ src/Pos.hs
@@ -1,16 +1,114 @@
 module Pos where
 
+import Debug.Trace
+import Control.Applicative
+import Data.Maybe
 
 data Pos = A | B | C | D | E | F | G | H deriving (Eq, Ord, Bounded, Enum, Show)
+
+mid :: Pos -> Maybe Pos
+mid s = Just s
+
+msucc :: Pos -> Maybe Pos
+msucc H = Nothing
+msucc s = Just $ succ s
+
+mpred :: Pos -> Maybe Pos
+mpred A = Nothing
+mpred s = Just $ pred s
+
+msucc2 :: Pos -> Maybe Pos
+msucc2 a = do 
+  a2 <- msucc a
+  msucc a2
+
+mpred2 :: Pos -> Maybe Pos
+mpred2 a = do
+  a2 <- mpred a
+  mpred a2
+
+
+
+type Dir = Pos -> Maybe Pos
+type Vec = (Dir, Dir)
+type Vecs = [Vec]
+
+vrook::Vecs
+vrook = [(mid, msucc),
+          (mid, mpred),
+          (msucc, mid),
+          (mpred, mid)]
+
+vbishop::Vecs
+vbishop = [(msucc, msucc),
+            (msucc, mpred),
+            (mpred, msucc),
+            (mpred, mpred)]
+
+vqueen::Vecs
+vqueen = vrook ++ vbishop
+
+vknight::Vecs
+vknight = [(msucc2, mpred),
+          (msucc2, msucc),
+          (mpred2, mpred),
+          (mpred2, msucc),
+          (msucc, mpred2),
+          (msucc, msucc2),
+          (mpred, mpred2),
+          (mpred, msucc2)]
+
+vapply:: Vec -> Coord -> Maybe Coord
+vapply (vf, vr) 
+  (Coord (File file) (Rank rank)) = do
+  file2 <- vf(file)
+  rank2 <- vr(rank)
+  return (Coord (File file2) (Rank rank2))
+
+vproject :: Vec -> Coord -> [Coord]
+vproject v coord = coord : rest
+  where rest = case vapply v coord of
+          Nothing -> []
+          Just c -> vproject v c
+
+-- [a3, a4, a5]
+-- [a3, a4]
+-- [a3, a4, a5]
+vpower :: [Coord] -> [[Coord]]
+vpower coords = foldl _power [] coords
+
+_power :: [[Coord]] -> Coord -> [[Coord]]
+_power [] coord = [[coord]]
+_power (x:rest) coord = (x ++ [coord]) : x : rest
+
+
+
+csvision :: [Coord] -> Maybe Vision
+csvision [] = Nothing
+csvision [_] = Nothing
+csvision coords = 
+  Just $ Vision
+  (head coords)
+  (last coords) (tail $ init coords)
+
+
+vecs :: Role -> Vecs
+vecs King = vqueen
+vecs Queen = vqueen
+vecs Rook = vrook
+  
+visions :: Piese -> [Vision]
+visions p = 
+  catMaybes $
+  map csvision $
+  (map ($ coord p) ls) >>= vpower
+  where ls =
+          (map vproject (vecs . role . piece $ p))
+
 
 data File = File Pos deriving (Eq, Ord)
 data Rank = Rank Pos deriving (Eq, Ord)
 
-toPath :: Pos -> Pos -> [Pos]
-toPath src dest
-  | src == dest = [src]
-  | src < dest = src : toPath (succ src) dest
-  | src > dest = src : toPath (pred src) dest
 
 data Coord = Coord { file :: File, 
                      rank:: Rank } deriving (Eq, Ord)
@@ -18,16 +116,8 @@ data Coord = Coord { file :: File,
 data Vision = Vision { origin :: Coord,
                        eyes :: Coord,
                        blocks :: [Coord]
-                     } deriving (Eq, Show)
+                     } deriving (Eq)
 
-
--- b7 e7 [b7 c7 d7 e7]
--- e7 b7 [e7 d7 c7 b7]
-
--- 7 7 [7]
--- 7 3 [7, 6, 5, 4, 3]
--- 3 5 [3, 4, 5]
--- 
 
 directVision :: Vision -> Bool
 directVision vSrc =
@@ -62,6 +152,11 @@ opposite :: Color -> Color
 opposite White = Black
 opposite Black = White
 
+projection :: Role -> Bool
+projection King = False
+projection _ = True
+
+
 data ColorMap a = ColorMap {
   white:: a,
     black:: a
@@ -70,6 +165,10 @@ data ColorMap a = ColorMap {
 instance Functor ColorMap where
   fmap fn (ColorMap white black) = 
     ColorMap (fn white) (fn black)
+
+instance Applicative ColorMap where
+  pure a = ColorMap a a
+  ColorMap wfs bfs <*> ColorMap w b = ColorMap (wfs w) (bfs b)
 
 
 colorMapFromList :: (a -> Color) -> [a] -> ColorMap [a]
@@ -85,6 +184,12 @@ data RoleMap a = RoleMap {
 instance Functor RoleMap where
   fmap fn (RoleMap king queen rook) = 
     RoleMap (fn king) (fn queen) (fn rook)
+
+
+instance Applicative RoleMap where
+  pure a = RoleMap a a a
+  RoleMap fk fq fr <*> RoleMap ak aq ar = 
+    RoleMap (fk ak) (fq aq) (fr ar)
 
 roleMapFromList :: (a -> Role) -> [a] -> RoleMap [a]
 roleMapFromList fn lst = RoleMap
@@ -103,6 +208,8 @@ whiteKing = Piece White King
 blackKing = Piece Black King
 whiteRook = Piece White Rook
 a4 = Coord (File A) (Rank D)
+a5 = Coord (File A) (Rank E)
+a6 = Coord (File A) (Rank F)
 b5 = Coord (File B) (Rank E)
 c4 = Coord (File C) (Rank D)
 
@@ -113,8 +220,7 @@ whiteRookc4 = Piese whiteRook c4
 isKing = (== King)
 
 board = [blackKingb5,
-         whiteKinga4,
-          whiteRookc4]
+         whiteKinga4]
 
 -- [Piese]
 -- Piese -> [Vision]
@@ -129,14 +235,20 @@ instance Functor CoroMap where
   fmap fn (CoroMap coro) = 
     CoroMap $ fmap (fmap fn) coro
 
+instance Applicative CoroMap where
+  pure a = CoroMap (pure (pure a))
+  CoroMap fgf <*> CoroMap fga = 
+    CoroMap ((<*>) <$> fgf <*> fga)
+
+flipColor :: CoroMap a -> CoroMap a
+flipColor (CoroMap (ColorMap w b)) = 
+  CoroMap $ ColorMap b w
+
+
 mapso :: [Piese] -> CoroMap [Piese]
 mapso lst = 
   CoroMap $ fmap (roleMapFromList (role.piece))
   (colorMapFromList (color.piece) lst)
-
-
-visions:: Piese -> [Vision]
-visions p = []
 
 -- f [a] -> (a -> [b]) -> f [b]
 -- CoroMap [Piese] -> CoroMap [Vision]
@@ -145,6 +257,7 @@ visions p = []
 -- CoroMap [Vision] -> CoroMap Check
 
 checkVision :: Vision -> Vision -> Maybe Check
+--checkVision vSrc vDest | trace (show vSrc ++ " " ++ show vDest) False = undefined
 checkVision vSrc vDest
   | captureVision vSrc vDest && 
     directVision vSrc = Just (Check vSrc)
@@ -156,8 +269,19 @@ checkVision vSrc vDest
 -- CoroMap [Vision] -> CoroMap [Vision -> Maybe Check]
                                 
 
--- board2 = (fmap . concatMap) visions $ mapso board
+boardVis :: CoroMap [Vision]
+boardVis = (fmap . concatMap) visions $ mapso board
 
+boardVisCheck :: CoroMap [Vision -> Maybe Check] 
+boardVisCheck = flipColor $ fmap (map checkVision) boardVis
+
+-- (Applicative f) => f [a] -> f [a -> b] -> f b
+
+bChecks :: CoroMap [Check]
+bChecks  = 
+  fmap catMaybes $ 
+  (liftA2 (zipWith ($)) boardVisCheck) $ 
+  boardVis
 
 
 
@@ -212,3 +336,10 @@ instance (Show a) => Show (RoleMap a) where
 
 instance (Show a) => Show (CoroMap a) where
   show (CoroMap comap) = show comap
+
+
+instance Show Check where
+  show (Check vision) = show vision ++ "+"
+
+instance Show Vision where
+  show (Vision origin eyes blocks) = show origin ++ "->" ++ show eyes
